97d615b7d6bb8f54c5f1c93a8e91911e3920c136
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-12-03 21:29:39 -0500

    remove caching (#11)

--- lib/evagg/content/__init__.py
+++ lib/evagg/content/__init__.py
@@ -1,13 +1,11 @@
 from .interfaces import ICompareVariants, IFindObservations, Observation, TextSection
 from .observation import ObservationFinder
 from .prompt_based import PromptBasedContentExtractor
-from .prompt_based_cache import PromptBasedContentExtractorCached
 from .variant import HGVSVariantComparator, HGVSVariantFactory
 
 __all__ = [
     # Content
     "PromptBasedContentExtractor",
-    "PromptBasedContentExtractorCached",
     # Interfaces.
     "ICompareVariants",
     "IFindObservations",
