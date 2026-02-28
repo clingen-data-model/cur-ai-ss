44c34d21bf289fef1e1448699e9baed117e8695e
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2026-02-27 19:15:01 -0600

    chore: refactoring ui

---
+++ lib/ui/paper/header.py
@@ -0,0 +1,99 @@
+import json
+import time
+
+import requests
+import streamlit as st
+
+from lib.agents.paper_extraction_agent import (
+    PaperExtractionOutput,
+)
+from lib.evagg.types.base import Paper
+from lib.models import ExtractionStatus, PaperResp
+from lib.ui.api import (
+    delete_paper,
+    get_http_error_detail,
+    get_paper,
+    requeue_paper,
+)
+
+st.set_page_config(layout='wide')
+
+paper_id = st.query_params.get('paper_id')
+if paper_id is None:
+    st.warning('No paper_id provided in URL.')
+    st.stop()
+
+with st.spinner('Loading paper...'):
+    try:
+        paper_resp: PaperResp = get_paper(paper_id)
+        if paper_resp is None:
+            st.stop()
+        if paper_resp.extraction_status != ExtractionStatus.PARSED:
+            st.write('Not yet parsed')
+            st.stop()
+        paper = Paper(id=paper_resp.id)
+        with open(paper.metadata_json_path, 'r') as f:
+            data = json.load(f)
+            paper_extraction_output: PaperExtractionOutput = (
+                PaperExtractionOutput.model_validate(data)
+            )
+    except requests.HTTPError as e:
+        st.error(f'Failed to load paper: {get_http_error_detail(e)}')
+    except Exception as e:
+        st.error(str(e))
+
+left, center, right = st.columns([1, 12, 1])
+with center:
+    st.markdown(f'# {paper_extraction_output.title}')
+    parts = [
+        f'{paper_extraction_output.first_author} et al. {paper_extraction_output.publication_year}'
+    ]
+
+    if paper_extraction_output.pmid:
+        parts.append(f'PMID: {paper_extraction_output.pmid}')
+
+    if paper_extraction_output.journal_name:
+        parts.append(paper_extraction_output.journal_name)
+
+    st.caption(' • '.join(parts))
+    st.divider()
+    left, right = st.columns([4, 2])
+    with left:
+        with st.container(horizontal=True, vertical_alignment='center'):
+            PAPER_PAGES = [
+                ('📄 PDF', 'pdf'),
+                ('📝 Paper Metadata', 'metadata'),
+                ('👤 Patients', 'patients'),
+                ('🧬 Variants', 'variants'),
+            ]
+            for i, (label, page) in enumerate(PAPER_PAGES):
+                if st.button(label, type='tertiary', width='content'):
+                    st.query_params['paper_id'] = paper_id
+                    st.switch_page(page)
+                if i < len(PAPER_PAGES) - 1:
+                    st.space('small')
+
+    with right:
+        with st.container(
+            horizontal=True, vertical_alignment='center', horizontal_alignment='center'
+        ):
+            if paper_resp.extraction_status == ExtractionStatus.PARSED:
+                st.badge('AI Parsing Success', icon=':material/check:', color='green')
+            elif paper_resp.extraction_status == ExtractionStatus.QUEUED:
+                st.badge('AI Parsing Queued', icon='⏳', color='yellow')
+            elif paper_resp.extraction_status == ExtractionStatus.FAILED:
+                st.badge('AI Parsing Failed', icon='❌', color='red')
+            if st.button('🔄 Rerun EvAGG', type='tertiary', width='content'):
+                try:
+                    requeue_paper(paper_id)
+                    st.toast('EvAGG Job Queued', icon=':material/thumb_up:')
+                    st.rerun()
+                except Exception as e:
+                    st.toast(f'Failed to requeue: {str(e)}', icon='❌')
+            if st.button('🗑️ Delete Paper', type='tertiary', width='content'):
+                try:
+                    delete_paper(paper_id)
+                    st.toast('Successfully deleted!', icon='🗑️')
+                    st.switch_page('dashboard.py')
+                except Exception as e:
+                    st.toast(f'Failed to delete: {str(e)}', icon='❌')
--- lib/ui/streamlit_app.py
+++ lib/ui/streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 pg = st.navigation(
     pages=[
         st.Page('dashboard.py'),
-        st.Page('details.py'),
+        st.Page('paper/header.py', title='Paper PDF'),
     ],
     position='hidden',
 )
