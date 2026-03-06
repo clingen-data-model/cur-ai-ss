1ba049b2995a1e68c26e5b506809db28ab83fde8
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2026-03-05 21:09:40 -0600

    feat: run upsert gene symbols as migration (#64)
    * Deletion of evagg code
    
    * Remove tests
    
    * ruff
    
    * ruff
    
    * remove coverage
    
    * rm
    
    * whittle away at paper class
    
    * test
    
    * still whittling
    
    * add back logging
    
    * logging
    
    * add assertions
    
    * add alembic migration
    
    * retype as Path
    
    * I think this is the best approach!
    
    * ruff
    
    * feat: run gene symbols as migration

---
+++ migrations/versions/eb6bb798b30f_upsert_gene_symbols.py
@@ -0,0 +1,41 @@
+"""upsert gene symbols
+
+Revision ID: eb6bb798b30f
+Revises: 794187dcb219
+Create Date: 2026-03-05 16:04:39.887523
+
+"""
+
+import traceback
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+from lib.evagg.utils.environment import env
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'eb6bb798b30f'
+down_revision: Union[str, None] = '794187dcb219'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    if env.SKIP_DATA_MIGRATIONS:
+        return
+    try:
+        from lib.reference_data.upsert_gene_symbols import main
+
+        main()
+    except Exception as e:
+        # Print full traceback for debugging
+        print('Error running upsert_gene_symbols migration:')
+        traceback.print_exc()
+        # Optionally, re-raise to fail the migration
+        raise
+
+
+def downgrade() -> None:
+    pass
