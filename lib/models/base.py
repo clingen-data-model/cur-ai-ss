e0f3701c773d43dd1bd015de09ce193497de70fd
Kyle Ferriter <kferrite@broadinstitute.org>
2026-05-07 10:54:20 -0400

    fix: share human edit note patch handling

--- lib/models/base.py
+++ lib/models/base.py
@@ -7,7 +7,7 @@ class Base(DeclarativeBase):
 
 
 class PatchModel(BaseModel):
-    def apply_to(self, obj: Base) -> None:
+    def apply_human_edit_notes(self, obj: Base) -> None:
         for field, value in self.model_dump(exclude_unset=True).items():
             if field.endswith('_human_edit_note'):
                 # Map human_edit_note field to evidence column
@@ -15,5 +15,9 @@ class PatchModel(BaseModel):
                 evidence_dict = getattr(obj, evidence_column, {}).copy()
                 evidence_dict['human_edit_note'] = value
                 setattr(obj, evidence_column, evidence_dict)
-            else:
+
+    def apply_to(self, obj: Base) -> None:
+        for field, value in self.model_dump(exclude_unset=True).items():
+            if not field.endswith('_human_edit_note'):
                 setattr(obj, field, value)
+        self.apply_human_edit_notes(obj)
