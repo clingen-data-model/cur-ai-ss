b05ef702632ba0eee8739e7b9e81aa74bae85adb
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2026-05-18 00:50:11 -0600

    bump worker limit

--- lib/bin/worker.py
+++ lib/bin/worker.py
@@ -19,7 +19,7 @@ from lib.tasks.models import TaskStatus, TaskType
 
 LEASE_TIMEOUT_S = 900
 POLL_INTERVAL_S = 10
-MAX_AGENTIC_TASKS = 15
+MAX_AGENTIC_TASKS = 20
 MAX_RETRIES = 2
 RETRY_DELAY_S = 30
 
