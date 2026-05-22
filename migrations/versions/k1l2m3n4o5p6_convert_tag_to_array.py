e7627c69bd705aa504143c06d156d84682759e75
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2026-05-22 12:04:34 -0600

    make it a no-op for now

--- migrations/versions/k1l2m3n4o5p6_convert_tag_to_array.py
+++ migrations/versions/k1l2m3n4o5p6_convert_tag_to_array.py
@@ -8,9 +8,6 @@ Create Date: 2026-05-22 12:00:00.000000
 
 from typing import Sequence, Union
 
-import sqlalchemy as sa
-from alembic import op
-
 # revision identifiers, used by Alembic.
 revision: str = 'k1l2m3n4o5p6'
 down_revision: Union[str, None] = 'j0k1l2m3n4o5'
@@ -19,14 +16,8 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    # Add the tags column
-    with op.batch_alter_table('papers', schema=None) as batch_op:
-        batch_op.add_column(
-            sa.Column('tags', sa.JSON(), nullable=False, server_default='[]')
-        )
+    pass
 
 
 def downgrade() -> None:
-    # Drop the tags column
-    with op.batch_alter_table('papers', schema=None) as batch_op:
-        batch_op.drop_column('tags')
+    pass
