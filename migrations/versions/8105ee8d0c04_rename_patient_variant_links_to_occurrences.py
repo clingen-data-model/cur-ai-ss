331ab378bf984955084a5fab9c3018ce9d1f1de6
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2026-05-25 15:51:52 -0600

    dump

--- migrations/versions/8105ee8d0c04_rename_patient_variant_links_to_occurrences.py
+++ migrations/versions/8105ee8d0c04_rename_patient_variant_links_to_occurrences.py
@@ -20,30 +20,17 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.rename_table('patient_variant_links', 'patient_variant_occurrences')
     with op.batch_alter_table('patient_variant_occurrences', schema=None) as batch_op:
-        # Update foreign key constraint for self-referential paired_variant_link_id
+        # Drop old indexes
+        batch_op.drop_index('ix_patient_variant_links_patient_id')
+        batch_op.drop_index('ix_patient_variant_links_variant_id')
+        batch_op.drop_index('ix_patient_variant_links_paired_variant_link_id')
+        # Drop old constraints
         batch_op.drop_constraint(
-            batch_op.f('fk_patient_variant_links_paired_variant_link_id'),
-            type_='foreignkey',
+            'uq_patient_variant_links_patient_variant', type_='unique'
         )
-        batch_op.create_foreign_key(
-            'fk_patient_variant_occurrences_paired_variant_link_id',
-            'patient_variant_occurrences',
-            ['paired_variant_link_id'],
-            ['id'],
-            ondelete='SET NULL',
-        )
-        # Update indexes
-        batch_op.drop_index(batch_op.f('ix_patient_variant_links_patient_id'))
-        batch_op.drop_index(batch_op.f('ix_patient_variant_links_variant_id'))
-        batch_op.drop_index(
-            batch_op.f('ix_patient_variant_links_paired_variant_link_id')
-        )
-        # Update unique constraint
-        batch_op.drop_constraint(
-            batch_op.f('uq_patient_variant_links_patient_variant'), type_='unique'
-        )
+        batch_op.drop_constraint('fk_patient_variant_links_paired', type_='foreignkey')
+        # Create new indexes with correct names
         batch_op.create_index(
             'ix_patient_variant_occurrences_patient_id', ['patient_id'], unique=False
         )
@@ -55,9 +42,17 @@ def upgrade() -> None:
             ['paired_variant_link_id'],
             unique=False,
         )
+        # Create new constraints with correct names
         batch_op.create_unique_constraint(
             'uq_patient_variant_occurrences_patient_variant',
             ['patient_id', 'variant_id'],
+        )
+        batch_op.create_foreign_key(
+            'fk_patient_variant_occurrences_paired',
+            'patient_variant_occurrences',
+            ['paired_variant_link_id'],
+            ['id'],
+            ondelete='SET NULL',
         )
 
 
